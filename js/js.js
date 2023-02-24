c31075a61aa39e5bfa5464681307cc65588f19d2
j-hyerim <112531998+j-hyerim@users.noreply.github.com>
2023-02-24 17:24:16 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,81 @@
+// function checkMobile(){
+ 
+//     var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+ 
+//     if ( varUA.indexOf('android') > -1) {
+//         //안드로이드
+//         alert("안드로이드임");
+//         return "android";
+//     } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+//         //IOS
+//         alert("아이폰임");
+//         return "ios";
+//     } else {
+//         //아이폰, 안드로이드 외
+//         return "other";
+//     }
+    
+// }
+// var mobile = (/iphone|ipad|ipod|android/i.test(navigator.userAgent.toLowerCase()));
+ 
+// if (mobile) { 
+//   //모바일 처리
+// }else{
+//   //비 모바일 처리
+// }
+$(document).ready(function(){
+
+    
+    // $("div").click(function(){
+    //     // alert("test");
+    //     console.log("tttt");
+
+    //     var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+    
+    //     if ( varUA.indexOf('android') > -1) {
+    //         //안드로이드;
+    //         console.log("android");
+    //         alert("안드로이드임");
+    //         return "android";
+    //     } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+    //         //IOS
+    //         console.log("ios");
+    //         alert("아이폰임");
+    //         return "ios";
+    //     } else {
+    //         //아이폰, 안드로이드 외
+    //         return "other";
+    //     }
+            
+    // });
+});
+
+
+// $(document).ready(function(){
+// $("div").click(function(){
+//     // alert("test");
+//     console.log("tttt");
+//     checkMobile();
+
+//     function checkMobile(){
+
+//         var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+    
+//         if ( varUA.indexOf('android') > -1) {
+//             //안드로이드;
+//             console.log("android");
+//             alert("안드로이드임");
+//             return "android";
+//         } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+//             //IOS
+//             console.log("ios");
+//             alert("아이폰임");
+//             return "ios";
+//         } else {
+//             //아이폰, 안드로이드 외
+//             return "other";
+//         }
+        
+//     }
+// });
+// });
